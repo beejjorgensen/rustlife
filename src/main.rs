04ccb6b45e2fc8646f9be03b789c78fae69e0923
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-12-29 16:28:29 -0800

    Move into struct

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,53 @@ use ratatui::{self, DefaultTerminal};
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main() -> Result<()> {
-    let terminal = ratatui::init();
-    let result = run(terminal);
-    ratatui::restore();
-    result
+struct App {
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(render)?;
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                if !handle_key_event(&key_event) {
-                    break;
-                }
-            }
+impl App {
+    fn new() -> Self {
+        Self {}
+    }
 
-            e => {
-                println!("{:#?}", e);
+    fn start(&self) -> Result<()> {
+        let terminal = ratatui::init();
+        let result = self.run(terminal);
+        ratatui::restore();
+        result
+    }
+
+    fn run(&self, mut terminal: DefaultTerminal) -> Result<()> {
+        loop {
+            terminal.draw(|frame| self.render(frame))?;
+
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    if !self.handle_key_event(&key_event) {
+                        break;
+                    }
+                }
+                _ => ()
             }
         }
+        Ok(())
     }
-    Ok(())
+
+    fn render(&self, frame: &mut ratatui::Frame) {
+        let x = frame.area().width / 2;
+        let y = frame.area().height / 2;
+        frame.set_cursor_position((x, y));
+    }
+
+    fn handle_key_event(&self, key_event: &KeyEvent) -> bool {
+        if key_event.code == KeyCode::Char('q') || key_event.code == KeyCode::Esc {
+            false
+        } else {
+            true
+        }
+    }
 }
 
-fn render(frame: &mut ratatui::Frame) {
-    let x = frame.area().width / 2;
-    let y = frame.area().height / 2;
-    frame.set_cursor_position((x, y));
-}
-
-fn handle_key_event(key_event: &KeyEvent) -> bool {
-    if key_event.code == KeyCode::Char('q') || key_event.code == KeyCode::Esc {
-        false
-    } else {
-        true
-    }
+fn main() -> Result<()> {
+    let app = App::new();
+    app.start()
 }
