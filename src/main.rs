e9d969d38a71c3e38d3427fa69da4e3920c3b3f0
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-12-29 16:38:52 -0800

    Arrow movement

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,31 @@ use ratatui::{self, DefaultTerminal};
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 struct App {
+    cursor_x: u16,
+    cursor_y: u16,
 }
 
 impl App {
     fn new() -> Self {
-        Self {}
+        Self {
+            cursor_x: 0,
+            cursor_y: 0,
+        }
     }
 
-    fn start(&self) -> Result<()> {
+    fn start(&mut self) -> Result<()> {
         let terminal = ratatui::init();
         let result = self.run(terminal);
         ratatui::restore();
         result
     }
 
-    fn run(&self, mut terminal: DefaultTerminal) -> Result<()> {
+    fn run(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        let term_size = terminal.size()?;
+
+        self.cursor_x = term_size.width / 2;
+        self.cursor_y = term_size.height / 2;
+
         loop {
             terminal.draw(|frame| self.render(frame))?;
 
@@ -35,21 +45,34 @@ impl App {
     }
 
     fn render(&self, frame: &mut ratatui::Frame) {
-        let x = frame.area().width / 2;
-        let y = frame.area().height / 2;
-        frame.set_cursor_position((x, y));
+        frame.set_cursor_position((self.cursor_x, self.cursor_y));
     }
 
-    fn handle_key_event(&self, key_event: &KeyEvent) -> bool {
-        if key_event.code == KeyCode::Char('q') || key_event.code == KeyCode::Esc {
-            false
-        } else {
-            true
+    fn handle_key_event(&mut self, key_event: &KeyEvent) -> bool {
+        match key_event.code {
+            KeyCode::Char('q') | KeyCode::Esc => {
+                return false;
+            }
+            KeyCode::Up => {
+                self.cursor_y -= 1;
+            }
+            KeyCode::Down => {
+                self.cursor_y += 1;
+            }
+            KeyCode::Left => {
+                self.cursor_x -= 1;
+            }
+            KeyCode::Right => {
+                self.cursor_x += 1;
+            }
+            _ => ()
         }
+
+        true
     }
 }
 
 fn main() -> Result<()> {
-    let app = App::new();
+    let mut app = App::new();
     app.start()
 }
