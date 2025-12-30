648ffb3b2355b3fd42d06358c2dc87deba48ffd6
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-12-30 15:26:31 -0800

    More rendering, keymapping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{self, DefaultTerminal};
+use ratatui::prelude::Stylize;
+use ratatui::symbols::border;
+use ratatui::widgets::Block;
+use ratatui::{self, DefaultTerminal, text::Line};
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -38,34 +41,78 @@ impl App {
                         break;
                     }
                 }
-                _ => ()
+                _ => (),
             }
         }
         Ok(())
     }
 
-    fn render(&self, frame: &mut ratatui::Frame) {
+    fn render(&mut self, frame: &mut ratatui::Frame) {
+        if self.cursor_y < 1 {
+            self.cursor_y = 1;
+        }
+
+        if self.cursor_y > frame.area().height - 2 {
+            self.cursor_y = frame.area().height - 2;
+        }
+
+        if self.cursor_x < 1 {
+            self.cursor_x = 1;
+        }
+
+        if self.cursor_x > frame.area().width - 2 {
+            self.cursor_x = frame.area().width - 2;
+        }
+
+        let title = Line::from(" Life ".bold());
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .border_set(border::THICK);
+
+        frame.render_widget(block, frame.area());
+
+        let fb = frame.buffer_mut();
+        fb[(self.cursor_x, self.cursor_y)].set_symbol("#");
+
         frame.set_cursor_position((self.cursor_x, self.cursor_y));
     }
 
     fn handle_key_event(&mut self, key_event: &KeyEvent) -> bool {
         match key_event.code {
-            KeyCode::Char('q') | KeyCode::Esc => {
+            KeyCode::Char('Q') | KeyCode::Esc => {
                 return false;
             }
-            KeyCode::Up => {
+
+            KeyCode::Up | KeyCode::Char('k') | KeyCode::Char('w') => {
                 self.cursor_y -= 1;
             }
-            KeyCode::Down => {
+            KeyCode::Down | KeyCode::Char('j') | KeyCode::Char('s') | KeyCode::Char('x') => {
                 self.cursor_y += 1;
             }
-            KeyCode::Left => {
+            KeyCode::Left | KeyCode::Char('h') | KeyCode::Char('a') => {
                 self.cursor_x -= 1;
             }
-            KeyCode::Right => {
+            KeyCode::Right | KeyCode::Char('l') | KeyCode::Char('d') => {
                 self.cursor_x += 1;
             }
-            _ => ()
+            KeyCode::Char('u') | KeyCode::Char('q') => {
+                self.cursor_x -= 1;
+                self.cursor_y -= 1;
+            }
+            KeyCode::Char('o') | KeyCode::Char('e') => {
+                self.cursor_x += 1;
+                self.cursor_y -= 1;
+            }
+            KeyCode::Char('n') | KeyCode::Char('z') => {
+                self.cursor_x -= 1;
+                self.cursor_y += 1;
+            }
+            KeyCode::Char(',') | KeyCode::Char('c') => {
+                self.cursor_x += 1;
+                self.cursor_y += 1;
+            }
+            _ => (),
         }
 
         true
