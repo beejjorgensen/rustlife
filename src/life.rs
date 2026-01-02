cecec66703ae8fda6155ffcbbab8ec6d327b5a95
Brian "Beej Jorgensen" Hall <beej@beej.us>
2026-01-02 10:54:06 -0800

    Add some doc comments

--- src/life.rs
+++ src/life.rs
@@ -1,11 +1,17 @@
+//! Conway's Life
+//!
+//! This Module implements the state management for Conway's Life. The implementation is a naive
+//! grid.
 use rand::random;
 
+/// Whether a cell is alive or dead.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum LifeCell {
     Dead,
     Alive,
 }
 
+/// Represents a Life grid.
 pub struct Life {
     width: usize,
     height: usize,
@@ -14,6 +20,7 @@ pub struct Life {
 }
 
 impl Life {
+    /// Create a new Life object.
     pub fn new() -> Self {
         Self {
             width: 0,
@@ -23,6 +30,7 @@ impl Life {
         }
     }
 
+    /// Initialize a Life object to a given width and height.
     pub fn init(&mut self, width: usize, height: usize) {
         self.cells[0].clear();
         self.cells[1].clear();
@@ -36,6 +44,7 @@ impl Life {
         }
     }
 
+    /// Fill the grid with random values.
     pub fn randomize(&mut self) {
         for y in 0..self.height {
             for x in 0..self.width {
@@ -54,6 +63,7 @@ impl Life {
         }
     }
 
+    /// Get the status of a cell.
     pub fn get_cell(&self, x: usize, y: usize) -> LifeCell {
         if x < self.width && y < self.height {
             self.cells[self.cur_page][y][x]
@@ -62,7 +72,8 @@ impl Life {
         }
     }
 
-    pub fn get_cell_weight(&self, x: usize, y: usize) -> i32 {
+    /// Get the weight of a cell, `1` for alive, `0` for dead.
+    fn get_cell_weight(&self, x: usize, y: usize) -> i32 {
         if x < self.width && y < self.height {
             if self.cells[self.cur_page][y][x] == LifeCell::Alive {
                 1
@@ -74,12 +85,14 @@ impl Life {
         }
     }
 
+    /// Set the status of a cell.
     pub fn set_cell(&mut self, x: usize, y: usize, state: LifeCell) {
         if x < self.width && y < self.height {
             self.cells[self.cur_page][y][x] = state;
         }
     }
 
+    /// Toggle the status of a cell.
     pub fn toggle(&mut self, x: usize, y: usize) {
         if x < self.width && y < self.height {
             match self.get_cell(x, y) {
@@ -89,6 +102,7 @@ impl Life {
         }
     }
 
+    /// Clear the life grid to Dead.
     pub fn clear(&mut self) {
         for row in &mut self.cells[self.cur_page] {
             row.fill(LifeCell::Dead);
@@ -96,6 +110,7 @@ impl Life {
     }
 
     /*
+    /// Get a row of the life grid.
     pub fn get_row(&self, y: usize) -> Option<&Vec<LifeCell>> {
         if y < self.height {
             Some(&(self.cells[y]))
@@ -105,6 +120,7 @@ impl Life {
     }
     */
 
+    /// Single step the life simuation.
     pub fn step(&mut self) {
         let other_page = if self.cur_page == 0 { 1 } else { 0 };
 
@@ -145,6 +161,9 @@ impl Life {
         self.cur_page = other_page;
     }
 
+    /// Resize the life grid.
+    ///
+    /// When shrinking, it truncates the values. When growing, it fills with dead cells.
     pub fn resize(&mut self, width: usize, height: usize) {
         for page in 0..=1 {
             self.cells[page].resize(height, vec![LifeCell::Dead; width]);
@@ -158,6 +177,7 @@ impl Life {
         self.height = height;
     }
 
+    /// Get a reference to all the cells.
     pub fn get_cells(&self) -> &Vec<Vec<LifeCell>> {
         &self.cells[self.cur_page]
     }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
+//! Various utility functions
 use ratatui::prelude::Rect;
 
+/// Clamp x, y coordinates to a [`Rect`].
 pub fn clamp_to_rect(x: u16, y: u16, rect: Rect) -> (u16, u16) {
     let max_x = rect.x + rect.width.saturating_sub(1);
     let max_y = rect.y + rect.height.saturating_sub(1);
@@ -7,6 +9,7 @@ pub fn clamp_to_rect(x: u16, y: u16, rect: Rect) -> (u16, u16) {
     (x.clamp(rect.x, max_x), y.clamp(rect.y, max_y))
 }
 
+/// Compute a new [`Rect`] from a given one and inset values.
 pub fn inset_rect(x_amt: u16, y_amt: u16, rect: Rect) -> Rect {
     Rect {
         x: rect.x + x_amt,
